8afaaa2456b0c0bf454fd9ba3575911c5b44964e
Tobias <tobias@olsson.be>
2018-10-10 08:41:21 +0200

    added curl-script for downloading from iso

--- src/build/groovy/downloadSubdivisions.groovy
+++ src/build/groovy/downloadSubdivisions.groovy
@@ -6,6 +6,8 @@ CountryCode.values().each {
     if (it.assignment == CountryCode.Assignment.OFFICIALLY_ASSIGNED) {
         String html = null
         try {
+
+// curl -v -v  -XPOST https://www.iso.org/obp/ui/ -d 'theme=iso-red&v-browserDetails=1&v-loc=https://www.iso.org/obp/ui/#iso:code:3166:GB' -H 'Content-Type: application/x-www-form-urlencoded' > foo.json
             html = new URL("http://www.unece.org/fileadmin/DAM/cefact/locode/Subdivision/${it.alpha2.toLowerCase(Locale.US)}Sub.htm").text
         } catch (IOException e) {
             println(e.getMessage())
